a8a8e136432ed743748771b3599bf04381e4de06
Ryan Kramer <rkramer613@gmail.com>
2017-01-17 18:02:27 -0700

    Fly Fisherman build

---
+++ code.js
@@ -0,0 +1,18 @@
+var flyFisherman = {
+    clothing: ['hat', 'vest', 'waders', 'rubber boots'],
+    set: {
+        rod: 'glass fiber',
+        reel: 'awesome5x2',
+        line: 'shark strength',
+        flies: [
+            {
+                nickname: 'little lady',
+                type: 'Sparkly'
+            },
+            {
+                nickname: 'dirty politician',
+                type: 'bribery'
+            }
+        ]
+    }
+}
